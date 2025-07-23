ce578099e947e5dae99924bd5a4d7af2f3e9d4bf
miraclepbspbs <304088570pbs@gmail.com>
2025-07-24 00:19:49 +0800

    init frontend

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    allowedHosts: [
+      'blog-frontend-428x.onrender.com'
+    ]
   }
 })
